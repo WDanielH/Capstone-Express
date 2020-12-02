1f2c683bc17b256a607ab9735ad770d0db8b211c
krishna rokhale <krishna.rokhale@gmail.com>
2020-12-01 17:25:25 -0800

    checking in

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// '/users' + '/'
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource ajhsgdjhsg');
+});
+
+// '/users' + '/name'
+router.get('/name', function(req, res, next) {
+  // console.log(req.query);
+  // console.log(req);
+
+  res.send('Its john!');
+  // setTimeout(() => {
+  //   res.send('Its john!');
+  // }, 5000)
+
+});
+
+router.get('/name/:id/:name', function(req, res, next) {
+  // console.log(req.query);
+  // console.log(req);
+  console.log(req.params);
+
+  res.send('Its params john!');
+  // setTimeout(() => {
+  //   res.send('Its john!');
+  // }, 5000)
+
+});
+
+router.post('/name', function(req, res, next) {
+  console.log(req.body);
+  res.send('Its post john!');
+})
+
+module.exports = router;
