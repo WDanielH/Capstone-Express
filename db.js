44ae8ca1a07799ce5850718ab8618a9d8c28df6d
WDanielH <warrendh@gmail.com>
2020-12-19 23:20:19 -0500

    inital commit

--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 const {Sequelize} = require('sequelize');
-const sequelize = new Sequelize('mysql://root:password@localhost/express-example', {logging: false});
+const sequelize = new Sequelize('mysql://root:W@rbear1!@localhost/express-example', {logging: false});
 module.exports = {sequelize};
